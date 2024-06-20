2f88fb20afbf936088340ded78914ec69045ac9f
logitec1994 <123654978a@gmail.com>
2024-06-20 21:30:29 +0300

    Base structure (not full)

---
+++ main.py
@@ -0,0 +1,10 @@
+from wallet import Wallet
+
+def main():
+    my_wall = Wallet()
+    print(my_wall.account._balance)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ user_account.py
@@ -0,0 +1,3 @@
+class UserAccount:
+    def __init__(self, balance: float) -> None:
+        self._balance: float = balance
