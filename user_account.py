e89fcd9238f3191d4ceb07dd5b207b3028f8f08b
logitec1994 <123654978a@gmail.com>
2024-07-19 04:45:29 +0300

    Initial commit: Set up Flask wallet application with user authentication

--- user_account.py
+++
@@ -1,3 +0,0 @@
-class UserAccount:
-    def __init__(self, balance: float) -> None:
-        self._balance: float = balance
--- wallet.py
+++
@@ -1,5 +0,0 @@
-from user_account import UserAccount
-
-class Wallet: # Should create UserAccount
-    def __init__(self, initial_capital: float = 0.0):
-        self.account = UserAccount(initial_capital)
